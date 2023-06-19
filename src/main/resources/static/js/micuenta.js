134d8715bdde3108ee50e28b9a20340b9d078b42
ManuelAmaya <manuel.amaya.acosta@hotmail.com>
2023-06-19 16:04:09 -0600

    Se imprime el resumen de compra en el pagina de Usuario y se conecta aplicacion base de datos MySQL en cloud server

--- src/main/resources/static/js/micuenta.js
+++ src/main/resources/static/js/micuenta.js
@@ -5,7 +5,8 @@ let nombre;
 let correo;
 const URL_MAIN = `http://127.0.0.1:8080/usuarios/${idUsuario}`;
 let botonCerrar = document.getElementById("botonCerrarSesion");
-
+let statusCompra = document.getElementById("statusCompra");
+let URL_COMPRAS = "http://127.0.0.1:8080/pedidos/";
 
 
 
@@ -24,16 +25,14 @@ if (localStorage.getItem('idUsuario')) {
         getUsuario().then((resultado) => {
             if (resultado) {
                 view();
+                mostrarCompra();
             } else {
-              
+
             }
         }).catch((error) => {
             console.log(error);
         });
     });
-} else {
-    // La clave "idUsuario" no existe en el local storage
-    
 }
 
 
@@ -65,3 +64,48 @@ function view() {
     const infocuenta = document.getElementById("infocuenta");
     infocuenta.innerHTML += itemHTML;
 }
+
+
+function mostrarCompra() {
+    var idUsuario = localStorage.getItem("idUsuario");
+  
+    fetch(URL_COMPRAS)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        stockCompras = json;
+        // Después de obtener el stock de productos, generamos el HTML de los productos
+        generarProductosHTML();
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
+  function generarProductosHTML() {
+    // Filtrar los objetos en "stockCompras" por la condición "id_Usuario === idUsuario"
+    var productosFiltrados = stockCompras.filter(function (producto) {
+      return producto.id_Usuario == idUsuario;
+    });
+  
+    // Crear una tabla en HTML para los productos filtrados
+    var tablaHTML = "<h3>Pedidos</h3><table>";
+    tablaHTML += "<tr><th>Nombre producto   </th><th>Precio </th><th>Cantidad   </th><th>Estado del pedido  </th></tr>";
+    productosFiltrados.forEach(function (producto) {
+      tablaHTML += "<tr>";
+      tablaHTML += "<td>" + producto.nombre + "</td>";
+      tablaHTML += "<td>" + producto.precio + "</td>";
+      tablaHTML += "<td>" + producto.cantidad + "</td>";
+      tablaHTML += "<td>En proceso</td>";
+      tablaHTML += "</tr>";
+    });
+    tablaHTML += "</table>";
+  
+    // Imprimir la tabla en el HTML
+    document.getElementById("statusCompra").innerHTML = tablaHTML;
+  }
+  
+  
+
+  
